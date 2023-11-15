4da84927c5b60c6faf024ab1438eee26aefc22e7
Arjen <arjen.siegers@ns.nl>
2023-11-15 21:16:09 +0000

    fix Docxingestor

--- src/Ingestor/Docxingestor.py
+++ src/Ingestor/Docxingestor.py
@@ -1,11 +1,13 @@
 from typing import List
 import docx
+from Ingestor import IngestInterface
 
 from .Quote import Quote
 
 class DOCXIngestor(IngestInterface):
     allowed_extensions = ['docx']
 
+    @classmethod
     def parse(cls, path) -> List[Quote]:
         if not cls.can_ingest:
             raise Exception('cannot ingest exception')
@@ -15,7 +17,8 @@ class DOCXIngestor(IngestInterface):
 
         for para in doc.paragraphs:
             if para.text != "":
-                parse = para.text.split(',')
+                print(para.text)
+                parse = para.text.split(' - ')
                 new_quote = Quote(parse[0],parse[1])
                 quotes.append(new_quote)
         return quotes
