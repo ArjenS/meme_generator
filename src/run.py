d43dca509727b5bd6aca6683abc20c00ce1362b0
Arjen <arjen.siegers@ns.nl>
2023-11-15 21:17:21 +0000

    temp run.py

---
+++ src/run.py
@@ -0,0 +1,8 @@
+from Ingestor import PDFIngestor
+
+
+#doc_file = './_data/DogQuotes/DogQuotesDOCX.docx'
+pdf_file = './_data/DogQuotes/DogQuotesPDF.pdf'
+
+#print(DOCXIngestor.parse(doc_file))
+print(PDFIngestor.parse(pdf_file))
