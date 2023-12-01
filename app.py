725280bedd0a9f346f0625d196b11457bcea7d57
Arjen <arjen.siegers@ns.nl>
2023-12-01 12:33:25 +0100

    some fixes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import random
 import os
 import requests
+import urllib
 from flask import Flask, render_template, abort, request
 from Ingestor import Ingestor
 from MemeEngine import MemeEngine
+from QuoteModel import QuoteModel
 
 
 app = Flask(__name__)
@@ -17,7 +19,7 @@ def setup():
     quote_files = [
         "./_data/DogQuotes/DogQuotesTXT.txt",
         "./_data/DogQuotes/DogQuotesDOCX.docx",
-        "./_data/DogQuotes/DogQuotesPDF.pdf",
+        #"./_data/DogQuotes/DogQuotesPDF.pdf",
         "./_data/DogQuotes/DogQuotesCSV.csv",
     ]
     
@@ -41,7 +43,7 @@ def meme_rand():
     """Generate a random meme"""
     img = random.choice(imgs)
     quote = random.choice(quotes)
-    path = meme.make_meme(img, quote.body, quote.author)
+    path = meme.make_meme(img, quote.body, quote.author)    
     return render_template("meme.html", path=path)
 
 
@@ -60,10 +62,15 @@ def meme_post():
     #    form param to a temp local file.
     # 2. Use the meme object to generate a meme using this temp
     #    file and the body and author form paramaters.
-    # 3. Remove the temporary saved image.
-    img = requests.get(url)
-    quote = requests.get(quote)   
-    path = meme.make_meme(img,quote.body,quote.author)
+    # 3. Remove the temporary saved image.       
+    # image_url = image_url
+    
+    img_url = request.form.get("img_url")
+    img_path = os.path.join('.','tmp','image.png')  #'.\tmp\image.png'
+    urllib.request.urlretrieve(img_url,img_path)
+        
+    quote = QuoteModel(request.form.get("body"),request.form.get("author"))
+    path = meme.make_meme(filename,quote.body,quote.author)
 
     return render_template("meme.html", path=path)
 
